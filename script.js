b3e1fc13b3995970fd1bf9cd1cda3894edb666ce
Chris <cclemmons@versatranz.com>
2021-02-16 10:30:23 -0500

    Added GSAP

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var controller = new ScrollMagic.Controller();
-
-new ScrollMagic.Scene({
-    duration:100,
-    offset:50
-}).setPin('#my-sticky-element')
-  .addTo(controller)
+gsap.to('.box', {
+    scrollTrigger : {
+        trigger:'.box',
+        start:'top bottom'
+    },
+    x:500,
+    ease:'expo.inOut',
+    duration:1
+})
